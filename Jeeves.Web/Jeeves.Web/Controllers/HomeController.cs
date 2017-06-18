c02af3c3ad4207914640270d3244ae4413f56423
John Reese <jpreese@mtu.edu>
2017-06-18 15:49:31 -0400

    display temps from db

--- Jeeves.Web/Jeeves.Web/Controllers/HomeController.cs
+++ Jeeves.Web/Jeeves.Web/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Jeeves.Web.Data;
+using Jeeves.Web.Models;
 
 namespace Jeeves.Web.Controllers
 {
@@ -15,7 +16,20 @@ namespace Jeeves.Web.Controllers
         /// <returns>The Home view.</returns>
         public ActionResult Index()
         {
-            return View();
+            if (Request.IsAuthenticated)
+            {
+                IEnumerable<Temperature> temps;
+                using (var db = new DataContext())
+                {
+                    temps = db.Temperatures.Take(5).OrderByDescending(m => m.ReadDate).ToList();
+                }
+
+                return View("_Dashboard", temps);
+            }
+            else
+            {
+                return View("_Login");
+            }
         }
     }
 }
